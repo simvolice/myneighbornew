a533897c68c0cb56216bcb2da08fcbb42a221732
Nikita Chmil <lecnnik@gmail.ru>
2016-12-31 14:44:16 +0600

    Добавил валидацию и комментарии для всех полей

--- api/Users.js
+++ api/Users.js
@@ -13,11 +13,32 @@ var Schema = mongoose.Schema;
 // create a schema
 var userSchema = new Schema({
 
-    FIO: { type: String, required: true, unique: true, min: 6, max: 30},
-    password: { type: String, required: true, unique: true, min: 32, max:32 },
+    //поле ФИО
+    FIO: { type: String, required: true, unique: true, min: 6, max: 30,
+
+        validateFio:{
+            validator: function (v) {
+                return /^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/.test(v);
+            },
+            message: '{VALUE} is not a valid FIO'
+        }
+    },
+
+    //поле пароля
+    password: { type: String, required: true, unique: true, min: 32, max:32,
+
+        validatePass: {
+            validator: function (v) {
+                return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v);
+            },
+            message: '{VALUE} is not a valid password'
+        }
+    },
+
+    //поле почты
     email: {type: String, required: true, unique: true, min: 6, max: 30,
 
-        validate: {
+        validateMail: {
             validator: function(v) {
                 return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v);
             },
@@ -28,25 +49,72 @@ var userSchema = new Schema({
     },
 
 
-
+    // координаты широта и долгота
     location: {
-
-        lat: {type: Number, required: true},
-        lon: {type: Number, required: true}
+        //широта
+        lat: {type: Number, required: true, min: 3, max: 12,
+            validateLat: {
+                validator: function(v) {
+                    return /^-?\d{1,3}\.\d+$/.test(v);
+                },
+                message: '{VALUE} is not a valid latitude'
+            }
+        },
+        //долгота
+        lon: {type: Number, required: true, min: 3, max: 12,
+            validateLon: {
+                validator: function(v) {
+                    return /^-?\d{1,3}\.\d+$/.test(v);
+                },
+                message: '{VALUE} is not a valid longitude'
+        }
 
 
     },
 
-    phone: {type: String, required: true, unique:true},
+    //поле телефонного номера
+    phone: {type: String, required: true, unique:true, min: 11, max: 12,
+        validatePhone: {
+            validator : function (v) {
+                return /^\d[\d\(\)\ -]{11,12}\d$/.test(v);
+            },
+            message: '{VALUE} is not phone'
+        }
+    }
+    },
 
+    // спектр оказания услуг
+    myServices: {type: Array, required: true, min: 1, max: 25},
 
-    myservices: {type: Array, required: true},
+    //аватар
+    avatar: {type: String, min:1, max:12,
+        validateMe: {
+            validator: function (v) {
+                return /^[a-zA-Z0-9]+$/.test(v);
+            },
+            message: '{VALUE} isn\'t info about User'
+        }
+    },
 
-    avatar: {type: String},
+    // рейтинг
+    rating: {type: Number, min: 1, max:5,
+        validateMe: {
+            validator: function (v) {
+                return /^[0-9]+$/.test(v);
+            },
+            message: '{VALUE} isn\'t info about User'
+        }
+    },
 
-    rating: {type: Number},
-
-    aboutme: {type: String}
+    // обо мне
+    aboutMe: {type: String, min:1, max: 300,
+        validateMe: {
+            validator: function (v) {
+                return /^[а-яА-ЯёЁa-zA-Z0-9]+$/.test(v);
+            },
+            message: '{VALUE} isn\'t info about User'
+        }
+    }
 
 
 
